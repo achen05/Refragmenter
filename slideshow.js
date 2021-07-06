1849cf9df2d732ed9adc12666f8669ddf3b68239
achen05 <72044476+achen05@users.noreply.github.com>
2021-07-05 21:41:55 -0700

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -3,14 +3,12 @@ function getRandomInt(max) {
 }
 
 function displayNextImage() {
-
-//  x = (x === images.length - 1) ? 0 : x + 1;
-x = getRandomInt(images.length)
-  document.getElementById("img").src = images[x];
-}
+              x = (x === images.length - 1) ? 0 : x + 1;
+              document.getElementById("img").src = images[x];
+          }
 
 function startTimer() {
-  setInterval(displayNextImage, 100);
+  setInterval(displayNextImage, 20);
 }
 
 var images = [],
@@ -20,3 +18,8 @@ images[1] = "images/2.png";
 images[2] = "images/3.png";
 images[3] = "images/4.png";
 images[4] = "images/5.png";
+images[5] = "images/6.png";
+images[6] = "images/7.png";
+images[7] = "images/8.png";
+images[8] = "images/9.png";
+images[9] = "images/10.png";
